9e0ff25096ad6d3f6418d8797df4503dee508e62
eames63 <jonathan@grangetec.com>
2025-01-14 12:12:12 +0100

    Missing semicolons added to query method namespaces

--- ClassTemplates/Template Files/GetViewModelByIdQuery.cs
+++ ClassTemplates/Template Files/GetViewModelByIdQuery.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using MediatR;
 using $rootnamespace$.$fileinputname$.Models;
 
-namespace $rootnamespace$.$fileinputname$.Queries
+namespace $rootnamespace$.$fileinputname$.Queries;
 
 	public record Get$fileinputname$ViewModelByIdQuery(int Id) : IRequest <List<$fileinputname$ViewModel>>;
 
